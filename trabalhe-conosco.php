929b85170886bdc7e4144dac8183c3291538ffce
Douglas Andrade <dglboy@gmail.com>
2021-12-28 19:21:18 -0300

    Aplicadado o overflow-hidden para as paginas de Solicite Orcamento e Trabalhe Conosco

--- trabalhe-conosco.php
+++ trabalhe-conosco.php
@@ -3,22 +3,26 @@ session_start();
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="pt-BR">
 
 <head>
-	<link rel="stylesheet" href="./css/style-menu.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
 	<meta http-equiv="Content-Language" content="pt-br, en, fr, it">
+	<meta charset="utf-8">
+
+
+	<link rel="stylesheet" href="./css/style-menu.css">
 	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 	<link rel="stylesheet" href="https://cdn.es.gov.br/fonts/font-awesome/css/font-awesome.min.css">
 	<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@600&display=swap" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<meta name="viewport" content="widht=device-widht, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="./css/style-form.css">
 	<link rel="stylesheet" href="./css/style-menu.css">
-	<meta charset="utf-8">
 
 	<title>Trabalhe Conosco</title>
 
+	<link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon" />
 </head>
 
 <body>
